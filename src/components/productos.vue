d23b6023288d8efd2b28c292d495c0e1671f4624
Samy Passu <samypassu10@gmail.com>
2024-06-19 14:00:06 -0500

    estilos de los productos video 10

--- src/components/productos.vue
+++ src/components/productos.vue
@@ -39,3 +39,48 @@ watchEffect(() => {
   );
 });
 </script>
+
+<style scoped>
+  .contenedor {
+    padding: 3rem;
+    margin-bottom: 2rem;  
+  }
+  .productos {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+  .producto {
+    display: flex;
+    align-items: center;
+  }
+  .informacion {
+    padding: 15px;
+  }
+  .titulo {
+    font-size: 1.25rem;
+  }
+  .descripcion {
+    color: gray;
+    word-wrap: break-word;
+  }
+  .foto {
+    max-width: calc(100% /4);
+    border-radius: 20px;
+    box-shadow: 0px 2px 4px rgb(76, 76, 76);
+  }
+  .precio {
+    color: #f9a72b;
+    font-weight: bold;
+    font-size: 1.25rem;
+  }
+  @media (max-width: 1024px) and (min-width: 768px) {
+    .productos {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+  @media (max-width: 768px) {
+    .productos {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  }
+</style>
