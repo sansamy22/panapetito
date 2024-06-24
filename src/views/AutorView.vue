0fd2ba378bc52a03da224350c396fe7ee2e60be2
Samy Passu <samypassu10@gmail.com>
2024-06-24 14:12:45 -0500

    plantilla del autor creada

---
+++ src/views/AutorView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="contenedor">
+        <div class="autor">
+            <img src="/imagenes/systemplus.png" alt="">
+            <p>Este proyecto fue desarrollado por: </p>
+            <h3>Samy passu Vargas</h3>
+            <p>Cursos de Diseño Web - SystemPlus 2024</p>
+            <a href="https://www.github.com/sansamy22" target="_blank">sansamy22</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .contenedor {
+        display: flex;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+    img {
+        max-width: 50%;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+</style>
